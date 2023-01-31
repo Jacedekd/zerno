c00ee77c844b029d30fa5b20b35b83b7120dfa3e
Aziz <azizkazanbaev@gmail.com>
2023-01-31 14:50:18 +0600

    commit

--- js/main.js
+++ js/main.js
@@ -1,3 +1,11 @@
+
+let axiosData;
+
+
+let flipCard__slide = document.querySelectorAll('.flipJs');
+flipCard__slide = [...flipCard__slide]
+
+
 let hamburger = document.querySelector(".hamburger"),
   hamburger_menu = document.querySelector(".hamburger_menu"),
   body = document.querySelector('body');
@@ -27,29 +35,39 @@ for (i = 0; i < acc.length; i++) {
     }
   });
 }
-let products__Block = document.querySelector('.products__Block')
-let products__BlockItem = document.querySelectorAll('.products__BlockItem')
-console.log(products__BlockItem);
-let flipCard__slide = document.querySelectorAll('.flipJs');
-flipCard__slide = [...flipCard__slide]
+let products__Block = document.querySelectorAll('.products__Block')
 
-// outerHTML
+console.log(products__Block);
+
+let products__BlockItem = document.querySelectorAll('.products__BlockItem')
+
 flipCard__slide.map((item) => {
   item.addEventListener('click', () => {
-    products__Block.classList.add('is_active')
-    if (item.outerHTML === "<div class=\"flipCard__slide flipJs\" style=\"background-image: url(/assets/img/product_1.png);\"></div>") {
-      products__BlockItem[2].classList.add('is_activeItem1')
+    products__Block[0].classList.remove('is_active')
+    products__Block[1].classList.remove('is_active')
+    products__Block[2].classList.remove('is_active')
+    if (item.dataset.url === `https://atlassoft.site/shrot/public/products/1`) {
+      products__Block[0].classList.add('is_active')
     }
-    if (item.outerHTML === "<div class=\"flipCard__slide flipJs modeY\" style=\"background-image: url(/assets/img/product_2.png);\"></div>") {
-      products__BlockItem[2].classList.add('is_activeItem2')
+    if (item.dataset.url === `https://atlassoft.site/shrot/public/products/6`) {
+      products__Block[1].classList.add('is_active')
     }
-    if (item.outerHTML === "<div class=\"flipCard__slide flipJs\" style=\"background-image: url(/assets/img/product_3.png);\"></div>") {
-      products__BlockItem[2].classList.add('is_activeItem3')
+    if (item.dataset.url === `https://atlassoft.site/shrot/public/products/7`) {
+      products__Block[2].classList.add('is_active')
     }
-
-
   })
 });
+
+
+
+
+// axios.all([axios.get(flipCard__slide[0].dataset.url), axios.get(flipCard__slide[1].dataset.url), axios.get(flipCard__slide[2].dataset.url)])
+//   .then((res) => {
+//     axiosData = res
+//     console.log(axiosData);
+//   })
+
+
 
 let vacanciesBtn = document.querySelectorAll('.vacanciesBtn');
 
@@ -100,3 +118,58 @@ if (product__video__Modal__Overlay != null) {
     console.log('o');
   })
 }
+
+let toolbar__phone = document.querySelector('.toolbar__phone')
+
+toolbar__phone.addEventListener('click', () => {
+  toolbar__phone.classList.add('is-active')
+})
+
+window.addEventListener('scroll', () => {
+  toolbar__phone.classList.remove('is-active')
+})
+
+let aboutUs__HistoryBlocking = document.querySelectorAll('.aboutUs__HistoryBlocking')
+// aboutUs__HistoryBlocking = Array.from(aboutUs__HistoryBlocking)
+
+let eHeight = [];
+
+// const amount = aboutUs__HistoryBlocking.reduce((total, item) => {
+
+// })
+let eght = 80;
+aboutUs__HistoryBlocking.forEach(e => {
+
+  eHeight.push(e.clientHeight)
+})
+console.log(eHeight);
+// aboutUs__HistoryBlocking.forEach(e => {
+//   if(e.classList.contains('ing2') === true) {
+//     e.style.top = `${eHeight[0] += 80}px `
+//   }
+//   if(e.classList.contains('ing3') === true) {
+//     e.style.top = `${eHeight[0] + 80 + eHeight[1]}px`
+//   }
+//   if(e.classList.contains('ing4') === true) {
+//     e.style.top = `${eHeight[0] + eHeight[1] + eHeight[2]}px`
+//   }
+//   if(e.classList.contains('ing5') === true) {
+//     e.style.top = `${eHeight[0] + eHeight[1] + eHeight[2] + eHeight[3]}px`
+//   }
+//   if(e.classList.contains('ing6') === true) {
+//     e.style.top = `${eHeight[0] + eHeight[1] + eHeight[2] + eHeight[3] + eHeight[4]}px`
+//   }
+//   if(e.classList.contains('ing7') === true) {
+//     e.style.top = `${eHeight[0] + eHeight[1] + eHeight[2] + eHeight[3] + eHeight[4] + eHeight[5]}px`
+//   }
+//   if(e.classList.contains('ing8') === true) {
+//     e.style.top = `${eHeight[0] + eHeight[1] + eHeight[2] + eHeight[3] + eHeight[4] + eHeight[5] + eHeight[6]}px`
+//   }
+//   // e.style.top = `${eHeight[1]}px`
+//   console.log(e);
+// })
+
+
+
+
+
--- js/swiper.js
+++ js/swiper.js
@@ -22,3 +22,39 @@ const swiper = new Swiper('.swiper', {
     slidesPerView: 2,
     // spaceBetween: 300,
 });
+
+const swiper2 = new Swiper('.swiper2', {
+    // Optional parameters
+    direction: 'horizontal',
+    loop: true,
+
+    // If we need pagination
+    pagination: {
+        el: '.swiper-pagination',
+    },
+
+    // Navigation arrows
+    navigation: {
+        nextEl: '.castom-button-next',
+        prevEl: '.castom-button-prev',
+    },
+
+    // And if we need scrollbar
+    scrollbar: {
+        el: '.swiper-scrollbar',
+    },
+
+    slidesPerView: 4,
+    breakpoints: {
+        // when window width is >= 320px
+        1030: {
+            slidesPerView: 4,
+        },
+        620: {
+            slidesPerView: 2,
+        },
+        300: {
+            slidesPerView: 1,
+        }
+    }
+});
